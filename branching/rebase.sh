c516574997ee2f7885111261fe7a2fc6ac7a9dce
Яковкено Михаил <zz.smirknt@sminco.ru>
2021-11-02 18:04:00 +0300

    main: @ insted *

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #display command line optinos
 count=1
-for param in "$*"; do
-    echo "\$* echo Parametr #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count+1 ))
 done
+
+echo "===="
