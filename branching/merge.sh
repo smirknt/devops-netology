77b3a34bd1368d2b0f530cdf5ce5b08cce372c93
Яковкено Михаил <zz.smirknt@sminco.ru>
2021-11-02 17:24:37 +0300

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #display command line optinos
 count=1
-for param in "$*"; do
-    echo "\$* echo Parametr #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parametr #$count = $param"
     count=$(( $count+1 ))
 done
